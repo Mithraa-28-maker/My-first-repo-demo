4ba6b2e8821b4a97ed39ce39b4997fd8d68c8777
Mithraa-28-maker <slmmithraa@gmail.com>
2026-03-15 23:19:50 +0530

    added arithmetic folder

---
+++ ARITHMETIC/Logical.py
@@ -0,0 +1,16 @@
+print(not False)
+print(not True)
+a = 20
+b = 30
+print(not (a > b)) #not operators
+
+val1 = True
+val2 = True
+print("AND operator:", val1 and val2) 
+#True tab aata hai jab dono true ho
+
+val3 = True 
+val4 = False
+print("OR operator:", val3 and val4) 
+#True aata hai jab koi ek true ho
+
---
+++ ARITHMETIC/typecasting.py
@@ -0,0 +1,5 @@
+#when we manually convert string to int or float or smtg
+a = 3.14
+a = str(a)
+
+print(type(a))
